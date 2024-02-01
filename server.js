eb11e694158bb6b470e92c53d3c6abc79dfc74d6
LampkinSynK <cejajaylin5@gmail.com>
2024-02-01 13:44:58 -0500

    Changed prompt

--- server.js
+++ server.js
@@ -95,7 +95,30 @@ app.post('/grade', async (req, res) => {
     const question = req.query.question;
     const answer = req.query.answer;
 
-    const prompt = `compare the answer to the question: ${question} and the answer: ${answer} give a percent rating of how accurate the answer was an an explanation of why the answer was rated that way`
+    const prompt = `
+        YOU ARE A PROFESSIONAL QUIZ QUESTION EVALUATOR
+        You will put your entire being into evaluating quizzes to the best of your ability
+        you will follow the following instructions to the letter
+        Do not add or remove anything outside of the instructions
+
+        you will compare the question: ${question}
+        to the answer: ${answer}
+
+        DO NOT BE EXTREMELY HARSH BE A BIT LENIENT BE FAIR AND BALANCED
+        ANSWERS DO NOT HAVE TO BE PERFECT AND DO NOT NEED ADDITIONAL CONTEXT; GUESS THE CONTEXT IF NECESSARY
+        DO NOT GIVE EXTREMELY LONG OR EXTREMELY SHORT EVALUATIONS
+        KEEP EVALUATIONS TO THE POINT CONTAIN WITHIN 2-3 SENTENCES AND DO NOT ADD ANY EXTRA INFORMATION
+        IF THE ANSWER IS WRONG PROVIDE THE CORRECT ANSWER WITHIN THE EXPLANATION
+
+        FORMAT YOUR RESPONSE AS FOLLOWS: "yes/no (percent) because (explanation)" THIS IS EXTREMELY IMPORTANT DO NOT ADD EXTRA INFORMATION
+    `
+    
+    
+    // `compare the answer to the question: ${question} 
+    // and the answer: ${answer} 
+    // give a percent rating of how accurate the answer was an an explanation of why the answer was rated that way
+    // Format your response as follows: "yes/no (percent) because (explanation)"
+    // `
 
     const options = {
 
@@ -129,13 +152,40 @@ app.post('/ask', async (req, res) => {
     const style = req.query.style;
 
     const prompt = 
+
     `
-        Create an open-ended ${length} question quiz
-        Make it about ${topic}
-        Make it on a ${expertise} level
-        Make it in a ${style} accent
-        Seperate questions by "~~", This is very important do not forget the "~~"
+        YOU ARE A QUIZMASTER YOUR ENTIRE EXISTENCE IS DEDICATED TO CREATING QUIZZES
+        You will put your entire being into creating the best quiz possible
+        you will follow the following instructions to the letter
+        Do not add or remove anything outside of the instructions
+
+        You will create a quiz about ${topic} assuming that a ${expertise} level of knowledge is required to answer the questions
+        The quiz will be in a ${style} accent and if it does not sound like it is in a ${style} accent you will rewrite it until it does
+        The quiz will be open-ended and will be ${length} questions long no more or no less
+        EACH QUESTION WILL BE SEPERATED BY "~~" THIS IS IMPORTANT DO NOT FORGET THE "~~"
+
+        the formatting in which you will write the quiz is as follows DO NOT ADD ANY EXTRA INFORMATION
+        NO EXTRA HEADERS AND NO EXTRA FOOTERS ONLY EXACTLY HOW WRITTEN:
+
+
+        1. write the first question here ~~
+        2. write the second question here ~~
+        3. write the third question here ~~
+        ...
+        ${length}. write the last question here
+        etc.
+
+
+        The quiz will be in a ${style} accent and if it does not sound like it is in a ${style} accent you will rewrite it until it does
+
     `
+    // `
+    //     Create an open-ended ${length} question quiz no more than ${length} and no less than ${length}
+    //     Make it about ${topic}
+    //     Make it on a ${expertise} level
+    //     Make it in a ${style} accent
+    //     Seperate questions by "~~", This is very important do not forget the "~~"
+    // `
 
     const options = {
         method: 'POST',
